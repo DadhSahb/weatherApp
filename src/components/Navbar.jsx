8220ac9273309e68dac7c0ae62e99a51f002b847
Hamza Hamid <hamza0995621@gmail.com>
2023-04-26 23:44:51 +0500

    weather-app

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a className="secondary"></a>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <strong>Weather App</strong>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <a className="secondary"></a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
